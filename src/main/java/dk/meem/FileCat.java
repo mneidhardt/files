57c05dec9a2f08d2809e8210ccac53914983354e
Michael Neidhardt <meidhardt@gmail.com>
2024-02-27 20:32:35 +0100

    Moved a few commas.

--- src/main/java/dk/meem/FileCat.java
+++ src/main/java/dk/meem/FileCat.java
@@ -6,13 +6,16 @@ public class FileCat {
 
 	
     public static void main(String[] args) throws IOException {
-        /*Writefile.bufferedWriter("./", "testfile.1.txt", 1000*1024*900);
+    	long t = System.currentTimeMillis();
+    	
+        Writefile.bufferedWriter("./", "testfile.1.txt", 1000*1024*900);
         Writefile.bufferedWriter("./", "testfile.2.txt", 1000*1024*900);
         Writefile.bufferedWriter("./", "testfile.3.txt", 1000*1024*900);
         Writefile.bufferedWriter("./", "testfile.4.txt", 1000*1024*900);
-        */
     	
     	String[] files = {"testfile.1.txt", "testfile.2.txt", "testfile.3.txt", "testfile.4.txt", "outfile.txt"};
     	MergeFiles.merge(files);
+    	
+    	System.out.println("Done in " + ((System.currentTimeMillis()-t)/1000) + " secs.");
 	}
 }
--- src/main/java/dk/meem/MergeFiles.java
+++ src/main/java/dk/meem/MergeFiles.java
@@ -30,6 +30,5 @@ public class MergeFiles {
 				}
 			}
 		}
-		System.out.println("DONE.");
 	}
 }
